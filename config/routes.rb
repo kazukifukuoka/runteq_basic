06b2365b4bb1b843b1e7d96d1071897178f24190
福岡和樹 <kazuki@mba.local>
2020-04-15 17:18:56 +0900

    sorcery_install

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
-  resources :users
+  resources :users, only: %i[new create]
   resources :user_sessions
 
   get 'login', to: 'user_sessions#new', as: :login
+  post 'login', to: 'user_sessions#create'
   post 'logout', to: 'user_sessions#destroy', as: :logout
 end
