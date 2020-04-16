db938f30eb89b19a4c22e1970cfd5bba173f8e8a
福岡和樹 <kazuki@mba.local>
2020-04-16 20:46:38 +0900

    boards_index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   def not_authenticated
-    redirect_to login_path, warning: t('.defaults.not_authenticated')
+    redirect_to login_path, warning: t('defaults.not_authenticated')
   end
 end
