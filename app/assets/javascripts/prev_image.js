cc1b05851e816a9c1113b1d798cbebcd93537308
福岡和樹 <kazuki@mba.local>
2020-04-17 17:54:48 +0900

    upload_board_image

--- app/assets/javascripts/prev_image.js
+++ app/assets/javascripts/prev_image.js
@@ -1,16 +1,14 @@
-$(document).on("ready turbolinks:load", function() {
-  $(function() {
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-    $('#img_prev').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $("#board_img").change(function(){
-        readURL(this);
-    });
+$(function() {
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+  $('#img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+      }
+  }
+  $("#board_img").change(function(){
+      readURL(this);
   });
-})
+});
