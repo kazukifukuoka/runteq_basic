b575898cba421717e2c914be50b24e544f2fe996
福岡和樹 <kazuki@mba.local>
2020-04-18 23:56:27 +0900

    board_edit_delete

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :set_user, only: %i[show edit destroy]
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
@@ -18,14 +19,34 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
     @comment = @board.comments.build
     @comments = @board.comments.all.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path(@board), success: t('.success')
+    else
+      flash[:danger] = t('.failed')
+      render :edit
+    end
+  end
+
+  def destroy
+    redirect_to boards_path, success: t('.success') if @board.destroy!
   end
 
   private
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image)
+  end
+
+  def set_user
+    @board = Board.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title(page_title = '')
+    base_title = 'RUNTEQ BOARD APP'
+
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
