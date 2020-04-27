47cbf85c3a81194faf9b4a7f35c5c3f564b73402
福岡和樹 <kazuki@mba.local>
2020-04-27 21:50:14 +0900

    password_reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,7 +2,7 @@ class PasswordResetsController < ApplicationController
   skip_before_action :require_login
   def create
     @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
+    @user&.deliver_reset_password_instructions!
     redirect_to root_path, success: 'パスワードのリセット手順を送信しました'
   end
 
