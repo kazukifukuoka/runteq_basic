b94b8af167fb265325bdef03145c2e04d8521e77
福岡和樹 <kazuki@mba.local>
2020-04-27 23:36:12 +0900

    password_reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,7 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
   def create
-    @user = User.find_by_email(params[:email]) # rubocop disable Rails/DynamicFindBy
+    @user = User.find_by_email(params[:email])
     @user&.deliver_reset_password_instructions!
     redirect_to root_path, success: 'パスワードリセット手順を送信しました'
   end
